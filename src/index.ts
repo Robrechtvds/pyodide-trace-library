7ec26193f13273e85498b06a65920d7ed156a354
Robrechtvds <Robrecht.vds@googlemail.com>
2022-12-16 13:21:53 +0100

    archive is now given as an argument untill we can serve it

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,18 @@
-import { loadPyodide } from "pyodide";
-import { readFileSync } from 'fs';
+import { loadPyodide, TypedArray } from "pyodide";
 
-async function run_python_code(code: string) {
+async function run_python_code(code: string, archive: TypedArray|ArrayBuffer, format: string) {
   let pyodide = await loadPyodide();
 
   //let zipResponse = await fetch("./assets/python.zip");
-  let data = readFileSync('src/assets/python.zip');
+  //let data = readFileSync('src/assets/python.zip');
 
-  // Slice (copy) its segment of the underlying ArrayBuffer
-  let ab = data.buffer.slice(data.byteOffset, data.byteOffset + data.byteLength);
-
-  pyodide.unpackArchive(ab, "zip");
+  pyodide.unpackArchive(archive, format);
   let pkg = pyodide.pyimport("code_example");
   return pkg.test_function(code);
 }
 
-export function generateTrace(code: string) {
+export function generateTrace(code: string, archive: TypedArray|ArrayBuffer, format: string) {
   console.log("Running code");
-  return run_python_code(code);
+
+  return run_python_code(code, archive, format);
 }
