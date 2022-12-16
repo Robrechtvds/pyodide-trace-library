cd6b0a728ecc30f898df1fedec90858b434a8129
Robrechtvds <Robrecht.vds@googlemail.com>
2022-12-16 16:22:41 +0100

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
 import { TypedArray, PyodideInterface } from "pyodide";
 
-async function run_python_code(code: string, pyodide: PyodideInterface, archive: TypedArray|ArrayBuffer, format: string) {
-
-  //let zipResponse = await fetch("./assets/python.zip");
-  //let data = readFileSync('src/assets/python.zip');
-
+export async function generateTrace(code: string, pyodide: PyodideInterface, archive: TypedArray|ArrayBuffer, format: string) {
   pyodide.unpackArchive(archive, format);
   let pkg = pyodide.pyimport("code_example");
   return pkg.test_function(code);
-}
-
-export function generateTrace(code: string, pyodide: PyodideInterface, archive: TypedArray|ArrayBuffer, format: string) {
-  console.log("Running code");
-
-  return run_python_code(code, pyodide, archive, format);
 }
